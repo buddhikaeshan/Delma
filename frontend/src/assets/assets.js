61c7fbb90b1eb263782aed65e94c0d658ab38e8a
Maleesha Pathirana <132542186+Maleeshapa@users.noreply.github.com>
2024-09-03 21:50:31 +0530

    assests

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -1,9 +1,9 @@
 import offer1 from './offer1.jpg'
 import logo from './logo.png'
-import back from './back.JPG'
-import room1 from './room1.JPG';
-import room2 from './room2.JPG';
-import room3 from './room3.JPG';
+import back from './back.jpg'
+import room1 from './room1.jpg';
+import room2 from './room2.jpg';
+import room3 from './room3.jpg';
 import hotel from './hotel.jpg';
 import dine from './dine.JPG';
 
